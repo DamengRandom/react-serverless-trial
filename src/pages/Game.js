ae73d308773532048bbf0ca693a47b6b1c45d040
Damon Wu <damonmaozewu@gmail.com>
2020-12-27 21:34:02 +1100

    complete game hook

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { StyledGame, StyledScore, StyledTimer, StyledCharacter } from '../styled/Game';
 import { Strong } from '../styled/Random';
 
 export default function Game({ history }) {
-  const [score, setScore] = useState(0);
   const MAX_SECONDS = 5;
+  const randomCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const [currentCharacter, setCurrentCharacter] = useState('');
+  const [score, setScore] = useState(0);
   const [ms, setMs] = useState(0);
   const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
+    setRandomCharacter();
     const currentTime = new Date();
     const interval = setInterval(() => updateTime(currentTime), 1);
 
@@ -22,7 +25,28 @@ export default function Game({ history }) {
       console.log('Game over ..');
       history.push('/gameOver');
     }
-  }, [seconds, ms, history])
+  }, [seconds, ms, history]);
+
+  const KeyUpHandler = useCallback((e) => {
+    console.log(e.key, currentCharacter)
+    if (e.key === currentCharacter) {
+      setScore(prevScore => prevScore + 1);
+    } else {
+      if (score > 0) {
+        setScore(prevScore => prevScore - 1);
+      }
+    }
+    setRandomCharacter();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentCharacter]);
+
+  useEffect(() => {
+    document.addEventListener('keyup', KeyUpHandler);
+    return () => {
+      document.removeEventListener('keyup', KeyUpHandler);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentCharacter]);
 
   const updateTime = (startTime) => {
     const endTime = new Date();
@@ -32,7 +56,7 @@ export default function Game({ history }) {
     const updatedSeconds = MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
     const updatedMs = 1000 - parseInt(formattedMSString.substring(formattedMSString.length - 3));
 
-    console.log(`${addLeadingZeros(updatedSeconds, 2)}: ${addLeadingZeros(updatedMs, 3)}`);
+    // console.log(`${addLeadingZeros(updatedSeconds, 2)}: ${addLeadingZeros(updatedMs, 3)}`);
     setSeconds(addLeadingZeros(updatedSeconds, 2));
     setMs(addLeadingZeros(updatedMs, 3));
   };
@@ -47,10 +71,15 @@ export default function Game({ history }) {
     return (zeros + num).slice(-length);
   };
 
+  const setRandomCharacter = () => {
+    const randomInt = Math.floor(Math.random() * 36);
+    setCurrentCharacter(randomCharacters[randomInt]);
+  };
+
   return (
     <StyledGame>
       <StyledScore>Score: <Strong>{score}</Strong></StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>{currentCharacter}</StyledCharacter>
       <StyledTimer>Timer: <Strong>{seconds}: {ms}</Strong></StyledTimer>
     </StyledGame>
   );

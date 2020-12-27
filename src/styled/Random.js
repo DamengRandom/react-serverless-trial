4c770a464e8a356f6f3cbb932664922820883fb8
Damon Wu <damonmaozewu@gmail.com>
2020-12-27 19:10:40 +1100

    styled home page

--- src/styled/Random.js
+++ src/styled/Random.js
@@ -3,3 +3,10 @@ import styled from 'styled-components';
 export const Accent = styled.span`
   color: #db2938;
 `;
+
+export const StyledTitle = styled.h1`
+  font-size: 3.2rem;
+  margin: bottom: 2rem;
+  text-align: center;
+  margin-top: 3rem;
+`;

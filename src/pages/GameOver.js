bd12afffb10c0e5c92533d92ae6de8b3f8c5a083
Damon Wu <damonmaozewu@gmail.com>
2020-12-28 17:30:59 +1100

    added save score into db by UI

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,5 +1,6 @@
-import React from 'react';
+
 import { useScore } from '../contexts/ScoreContext';
+import { Strong } from '../styled/Random';
 
 export default function GameOver({ history }) {
   const [score] = useScore();
@@ -7,9 +8,28 @@ export default function GameOver({ history }) {
   if (score === -1) {
     history.push('/');
   }
+
+  const saveHighScore = async () => {
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          name: 'trial 01',
+          score,
+        })
+      }
+      const res = await fetch('/.netlify/functions/saveHighScore', options);
+      const data = await res.json();
+      console.log('made it: ', data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
-      GameOver: {score}
+      <p>GameOver: <Strong>{score}</Strong></p>
+      <button onClick={saveHighScore}>Save your record</button>
     </div>
   );
 };
